e98ef9694b0e9b3e71f82085bd28db063ef1216b
MohammedJasim001 <jasimat001@gmail.com>
2025-07-15 10:05:04 +0530

    add like and comment in blog

--- src/routes/blog.routes.ts
+++ src/routes/blog.routes.ts
@@ -1,10 +1,13 @@
 import express from 'express'
-import { createBlog, fetchBlog } from '../controllers/blogController'
+import { addComment, createBlog, fetchBlog, getComments, toggleLike } from '../controllers/blogController'
 import userAuth from '../middlewares/authMiddleware'
 
 const routes = express.Router()
 
 routes.post('/create',userAuth, createBlog)
 routes.get('/get',userAuth,fetchBlog)
+routes.patch('/:blogId/like', userAuth, toggleLike)
+routes.post('/:blogId/comment/post-comments',userAuth, addComment)
+routes.get('/:blogId/comment/get-comments',userAuth,getComments)
 
 export default routes
